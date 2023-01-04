e0c4a419452532e9a0ecf7bee0344bf7d3ebc67c
Phil <phil.wright@componentfactory.com>
2023-01-04 23:13:12 +1100

    Initial

---
+++ 0001_string_literals/Program.cs
@@ -0,0 +1,92 @@
+﻿//----------------------------------------
+// Challenge
+//----------------------------------------
+
+var _ = $$$""""
+        Lorem ipsum dolor sit amet, consectetur adipiscing 
+        elit, sed do eiusmod tempor incididunt ut labore et 
+        dolore magna aliqua. Ut enim ad minim veniam, quis 
+        nostrud exercitation ullamco laboris nisi ut aliquip 
+        ex ea commodo consequat.
+        """";   
+
+//----------------------------------------
+// 1 - Quoted literal
+//----------------------------------------
+
+var a = "Great for single line text without escaped characters";
+var b = "C:\\YouTube\\Data\\Documents\\";
+
+
+//----------------------------------------
+// 2 - Verbatim literal
+//----------------------------------------
+
+var c = @"C:\YouTube\Data\Documents\";
+
+var d = @"Shall I compare Thee to a Summers day?
+Thou art more lovely and more temperate:";
+
+var e = @"Shall I compare ""Thee"" to a Summers day?
+Thou art more lovely and more temperate:";
+
+
+//----------------------------------------
+// 3 - Raw literal
+//----------------------------------------
+
+var f = """
+Shall I compare "Thee" to a Summers day?
+Thou art more lovely and more temperate:
+""";
+
+var g = """
+        Shall I compare "Thee" to a Summers day?
+        Thou art more lovely and more temperate:
+        """;
+
+var h = """"
+        Shall I compare """Thee""" to a Summers day?
+        Thou art more lovely and more temperate:
+        """";
+
+
+//----------------------------------------
+// 4 - Interpolated quoted literal
+//----------------------------------------
+
+var i = $"C:\\YouTube\\Data\\{DateTime.Now.DayOfWeek}\\";
+
+//----------------------------------------
+// 5 - Interpolated verbatim literal
+//----------------------------------------
+
+var j = $@"C:\YouTube\Data\{DateTime.Now.DayOfWeek}\";
+var k = @$"C:\YouTube\Data\{DateTime.Now.DayOfWeek}\";
+
+//----------------------------------------
+// 6 - Interpolated raw literal
+//----------------------------------------
+var l = $""""
+        Shall I compare Three to a {DateTime.Now.DayOfWeek}?
+        Thou art more lovely and more temperate:
+        """";
+
+var m = $$""""
+        How can I use curly brackets {like this} on a {{DateTime.Now.DayOfWeek}}?
+        """";   
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+
---
+++ COMMANDS.md
@@ -0,0 +1,15 @@
+# Solution
+dotnet new sln -n CSharpProfessional
+
+# .gitignore
+dotnet new gitignore
+
+# .editorConfig
+copied from github dotnet/runtime/.editorconfig
+
+# Add a new project, example
+dotnet new console -o ./0001_string_literals   
+dotnet sln add ./0001_string_literals   
+
+# CodeLens
+File -> Preferences -> Settings -> csharp.referencesCodeLens.enabled
