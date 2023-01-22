d67441b25d87559cb3a0574bc6f002a7a4915d08
Phil <phil.wright@componentfactory.com>
2023-01-22 13:50:44 +1100

    Add syntactic sugar

---
+++ 0004_syntactic_sugar/Program.cs
@@ -0,0 +1,93 @@
+﻿
+//-------------------------------
+// foreach over an Enumerator
+//-------------------------------
+var x = new List<int> { 1, 2, 3 };
+foreach(var i in x)
+    Console.WriteLine(i);
+
+// Rewritten code
+IEnumerator<int> enumerator = x.GetEnumerator();
+try
+{
+    while (enumerator.MoveNext())
+    {
+        int i = enumerator.Current;
+        Console.WriteLine(i);
+    }
+}
+finally
+{
+    if (enumerator != null)
+        enumerator.Dispose();
+}
+
+
+//-------------------------------
+// foreach over an Array
+//-------------------------------
+var x = new int[] { 1, 2, 3 };
+foreach(var i in x)
+    Console.WriteLine(i);
+
+// Rewritten code
+int num = 0;
+while (num < x.Length)
+{
+    int i = x[num];
+    Console.WriteLine(i);
+    num++;
+}  
+
+
+//-------------------------------
+// Lamba assigned to an Action
+//-------------------------------
+Action<string> z1 = (name) => Console.WriteLine(name);
+
+// Rewritten code
+Action<string> z2 = Lambda.Method ?? (Lambda.Method = new Action<string>(Lambda.Singleton.Body));
+
+
+//-------------------------------
+// Closure assigend to an Action
+//-------------------------------
+var prefix = "Yo ";
+Action<string> x1 = (name) => Console.WriteLine(prefix + name);
+var prefix = "Hi ";
+x1("Alice");
+
+// Rewritten code
+Closure c = new Closure();
+c.prefix = "Yo ";
+Action<string> x2 = new Action<string>(c.Body);
+c.prefix = "Hi ";
+x2("Alice");
+
+
+//-------------------------------
+// Lamba assigned to an Action
+//-------------------------------
+internal sealed class Lambda
+{
+    public static readonly Lambda Singleton = new Lambda();
+    public static Action<string> Method;
+
+    internal void Body(string name)
+    {
+        Console.WriteLine(name);
+    }
+}
+
+//-------------------------------
+// Closure assigend to an Action
+//-------------------------------
+internal sealed class Closure
+{
+    public string prefix;
+
+    internal void Body(string name)
+    {
+        Console.WriteLine(string.Concat(prefix, name));
+    }
+}
--- COMMANDS.md
+++ COMMANDS.md
@@ -8,8 +8,9 @@ dotnet new gitignore
 copied from github dotnet/runtime/.editorconfig
 
 # Add a new project, example
-dotnet new console -o ./0003_record_internals
-dotnet sln add ./0003_record_internals
+dotnet new console -o ./0004_syntactic_sugar
+dotnet sln add ./0004_syntactic_sugar
+cd ./0004_syntactic_sugar
 
 # CodeLens
 File -> Preferences -> Settings -> csharp.referencesCodeLens.enabled
